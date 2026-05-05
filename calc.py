c9334897021734971d843ee81ff6a8d9655f569f
Mixail-web <viktors274@gmail.com>
2026-05-05 22:17:39 +0600

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,6 @@
-# Автор: ...
+# Автор: Вастьянов Михаил
+
+import math
 
 def add(a, b):
     return a + b
@@ -9,10 +11,14 @@ def subtract(a, b):
 
 
 def multiply(a, b):
-    # TODO: не реализовано
-    return None
+    return a * b
+
+
+def sqrt(x):
+    return math.sqrt(x)
 
 
 if __name__ == "__main__":
     print("Простой калькулятор запущен.")
     print(f"2 + 2 = {add(2, 2)}")
+
